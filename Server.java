30b5e64c7526f3a82c78a4b7951bb2e417636c4d
nksacayan <nksacayan@gmail.com>
2019-12-07 20:13:29 -0800

    Redid Server.java to user ClientHandler class and added ArrayList for clients

--- Server.java
+++ Server.java
@@ -1,16 +1,16 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
-public class Server implements Runnable {
+public class Server {
 
     // port 48620-49150 is unassigned range
     static final int PORT_NUMBER = 48620;
-    Socket connectionSocket = null;
-    ObjectOutputStream outToClient = null;
-    ObjectInputStream inFromClient = null;
 
+/*
     public Server(Socket connectionSocket) throws IOException {
         this.connectionSocket = connectionSocket;
         this.outToClient = new ObjectOutputStream(this.connectionSocket.getOutputStream());
@@ -18,24 +18,75 @@ public class Server implements Runnable {
         System.out.println("Server initiated");
 
     }
+*/
 
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
+
+        // ArrayList for keeping track of clients
+        ArrayList<ClientHandler> clientList = new ArrayList<>();
+
+        // index for clients
+        int i = 0;
 
         // Initialize welcome socket
         ServerSocket welcomeSocket = new ServerSocket(PORT_NUMBER);
+        Socket connectionSocket;
 
-        // Accept connection and spawn thread
-        while(true) {
-            Server server = new Server(welcomeSocket.accept());
-            Thread chatThread = new Thread(server);
-            chatThread.start();
+        // Repeatedly accept new connections
+        while (true) {
+            // Accept connection
+            connectionSocket = welcomeSocket.accept();
+            System.out.println("Client connected: " + connectionSocket);
+
+            // Get I/O streams for handler class
+            ObjectInputStream inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
+            ObjectOutputStream outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
+
+            // Create handler object
+            ClientHandler client = new ClientHandler(connectionSocket, "client " + i++, outToClient, inFromClient);
+
+            // Spawn thread
+            Thread clientThread = new Thread(client);
+
+            // Add client to list
+            clientList.add(client);
+
+            // Start thread
+            clientThread.start();
         }
 
     }
 
+}
+
+class ClientHandler implements Runnable {
+
+    Scanner scan = new Scanner(System.in);
+    private String username;
+    final ObjectOutputStream outToClient;
+    final ObjectInputStream inFromClient;
+    Socket connectionSocket;
+
+    public ClientHandler(Socket connectionSocket, String username,
+                         ObjectOutputStream outToClient, ObjectInputStream inFromClient) {
+        this.connectionSocket = connectionSocket;
+        this.outToClient = outToClient;
+        this.inFromClient = inFromClient;
+        this.username = username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
     @Override
     public void run() {
+
         // Send welcome message
         Message msg = new Message();
         msg.setMsg("Welcome!");
@@ -49,9 +100,7 @@ public class Server implements Runnable {
         // Receive username
         try {
             msg = (Message) inFromClient.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         String username = msg.getMsg();
@@ -71,9 +120,7 @@ public class Server implements Runnable {
         // Wait and receive message from client
         try {
             msg = (Message) inFromClient.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -82,9 +129,7 @@ public class Server implements Runnable {
         // Receive sign off message from client
         try {
             msg = (Message) inFromClient.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -97,50 +142,5 @@ public class Server implements Runnable {
         }
 
 
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // below this point is copy pasted and not yet edited
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- /*       try {
-            is = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            os = new PrintWriter(s.getOutputStream());
-
-        } catch (IOException e) {
-            System.out.println("IO error in server thread");
-        }
-
-        try {
-            line = is.readLine();
-            while (line.compareTo("QUIT") != 0) {
-
-                os.println(line);
-                os.flush();
-                System.out.println("Response to Client:  " + line);
-                line = is.readLine();
-            }
-        } catch (IOException e) {
-
-            line = this.getName();
-        } catch (NullPointerException e) {
-            line = this.getName();
-
-        } finally {
-            try {
-                System.out.println("Connection Closing..");
-                if (is != null) {
-                    is.close();
-
-                }
-
-                if (os != null) {
-                    os.close();
-                }
-                if (s != null) {
-                    s.close();
-                }
-
-            } catch (IOException ie) {
-                System.out.println("Socket Close Error");
-            }
-        }//end finally */
     }
 }
